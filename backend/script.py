ebe0abb71c96c7fd6fab6f0c22847fa6147f850d
Victor Grillo <v.grillo321@gmail.com>
2023-09-16 17:31:58 -0400

    Changes to pass all the data of json file to a dictionary and added average price per sqft calculation

--- backend/script.py
+++ backend/script.py
@@ -3,53 +3,59 @@ import urllib
 import json
 
 
-state = input("Please add the state acronym (ex. FL)") #TODO: Change to a lambda compatible input
-cityinput= input("Please add the city (optional)")     #TODO: Same
-city = ""
+# state = input("Please add the state acronym (ex. FL)") #TODO: Change to a lambda compatible input
+# cityinput= input("Please add the city (optional)")     #TODO: Same
+# city = ""
 
 # Create query string; create a function that can be applied to other values
-for i in range(len(cityinput)):
-    if cityinput[i] == " ":
-        city+="%20"
-        continue
-    city += cityinput[i]
+# for i in range(len(cityinput)):
+#     if cityinput[i] == " ":
+#         city+="%20"
+#         continue
+#     city += cityinput[i]
     
-url_q = "city="+city+"&state="+state
+# url_q = "city="+city+"&state="+state
 
-url = "https://api.rentcast.io/v1/listings/rental/long-term?"+url_q
+# url = "https://api.rentcast.io/v1/listings/rental/long-term?"+url_q
 
-headers = {
-    "accept": "application/json",
-    "X-Api-Key": "4bbec6c728f54cfd9e408d03c65e9870" # TODO: Pass API key safer
-}
+# headers = {
+#     "accept": "application/json",
+#     "X-Api-Key": "4bbec6c728f54cfd9e408d03c65e9870" # TODO: Pass API key safer
+# }
 
 # Call API
-response = requests.get(url, headers=headers)
+# response = requests.get(url, headers=headers)
 
 # Add response as text data
-data = response.text
+# data = response.text
 
-#Pass Json data to spreadsheet if it contains the squareFootage
-row_counter = 9
+#Pass Json data to List or Dictionary if it contains the squareFootage
+row_counter = 9 #TODO: add as input
 jsonObjectNumber = 0
 
-while row_counter < 19:
+with open("test_data.json", "r") as openfile: #TODO:change to sample.json when using API
+    json_object = json.load(openfile)
+
+rentalSampleDict = {"address":"", "price":0, "size":0, "pricesqft":0}
+rentalPricePerSQFT = []
+while row_counter < 2:
 
     address = json_object[jsonObjectNumber]["addressLine1"]
     price = json_object[jsonObjectNumber]["price"]
     
     if not (json_object[jsonObjectNumber].get('squareFootage') is None):
         sqft = json_object[jsonObjectNumber]["squareFootage"]
-        rentalPricePerSQFT = calculatePAM(address, price, sqft) #TODO: Pass each one to front end or field on API
+        pricePerSqft=price/sqft
+        rentalPricePerSQFT.insert(pricePerSqft) #TODO: Pass each one to front end or field on API
         row_counter+=1
     
+    rentalSampleDict = {"address":address, "price":price, "size":sqft, "pricesqft":rentalPricePerSQFT}
+    print (rentalSampleDict)
     jsonObjectNumber+=1
+ 
 
 
-#Function to calculate PAM
-def calculatePAM(address,price,size):
-    pam = price/size
-return pam
+print (rentalPricePerSQFT)
 
 
 
