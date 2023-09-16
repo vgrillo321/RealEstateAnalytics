fee6beb89d66eeefbbf3b772abbaee73d5b329a1
Victor Grillo <v.grillo321@gmail.com>
2023-09-16 18:22:34 -0400

    Pass API key safer

--- backend/script.py
+++ backend/script.py
@@ -1,15 +1,15 @@
 import requests
 import urllib
 import json
-# from dotenv import load_dotenv Enable when testing API call
+from dotenv import load_dotenv
 import os
 
 state = input("Please add the state acronym (ex. FL)") #TODO: Change to a lambda compatible input
 cityinput= input("Please add the city (optional)")     #TODO: Same
 city = ""
 
-# def configure():
-#     load_dotenv()
+def configure():
+    load_dotenv()
 
 # Create query string; create a function that can be applied to other values
 for i in range(len(cityinput)):
@@ -22,18 +22,20 @@ url_q = "city="+city+"&state="+state
 
 url = "https://api.rentcast.io/v1/listings/rental/long-term?"+url_q
 
-# headers = {
-#     "accept": "application/json",
-#     "X-Api-Key": os.getenv('api-key') # TODO: Pass API key safer
-# }
+#TODO: Modify to pass key from .env file
+api_key=os.environ.get('RENTCAST_API_KEY')
+
+headers = {
+    "accept": "application/json",
+    "X-Api-Key": api_key # TODO: Pass API key safer
+}
 
 # Call API
-# response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers)
 
 # Add response as text data
-# data = response.text
+data = response.text
 
-#Pass Json data to spreadsheet if it contains the squareFootage
 loopCounter = 0 #TODO: add as input depending on the sample data needed
 jsonObjectNumber = 0
 list_counter=0
@@ -41,7 +43,12 @@ list_counter=0
 # print(os.getcwd())
 averagePricePerSQFT=0
 
-with open("sample.json", "r") as openfile: #TODO:change to sample.json when using API
+# Load json data into a variable
+with open("sample.json", "w") as openfile:
+    openfile.write(data)
+
+# Open the sample file
+with open("sample.json", "r") as openfile: 
     json_object = json.load(openfile)
 
     rentalSampleDict = {}
@@ -64,7 +71,7 @@ with open("sample.json", "r") as openfile: #TODO:change to sample.json when usin
         
         jsonObjectNumber+=1
 
-print("Im outside the loop now ----------------------")
+print("Im outside the json file now ----------------------")
 print(rentalSampleDict)
 averagePricePerSQFT= round(averagePricePerSQFT/list_counter, 2)
 print ("The average price per sqft in the area is: " + str(averagePricePerSQFT))
